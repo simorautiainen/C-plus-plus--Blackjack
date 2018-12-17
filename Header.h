561e970fd2bf49b5cf351aef51ad016ed5aac6b1
simorautiainen <43709265+simorautiainen@users.noreply.github.com>
2018-12-17 17:30:36 +0200

    Create Header.h

---
+++ Header.h
@@ -0,0 +1,12 @@
+#pragma once
+#include<string>
+using namespace std;
+
+
+void peli(string[], int[], string[],int[],int&,string[],int[],int&);
+void alasconvertteri(string&);
+void pelikortinjako(string[], int[], string[], int[], int&);
+void viivojenprinttaus(int mode=0);
+void arvojen_pisteleminen(string[], string[], int[], int[]);
+extern string muokkaamattomat[];
+extern int muokkaamattomatarvot[];
